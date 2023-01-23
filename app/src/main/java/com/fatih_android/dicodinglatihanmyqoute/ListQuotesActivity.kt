59204710cdc4b6765375b008b1fd17090fe4ddda
fatih <muhammadfatihmaulana434@gmail.com>
2023-01-23 10:44:33 +0700

    Initial commit

---
+++ app/src/main/java/com/fatih_android/dicodinglatihanmyqoute/ListQuotesActivity.kt
@@ -0,0 +1,93 @@
+package com.fatih_android.dicodinglatihanmyqoute
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.fatih_android.dicodinglatihanmyqoute.adapter.QuoteAdapter
+import com.fatih_android.dicodinglatihanmyqoute.databinding.ActivityListQuotesBinding
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import org.json.JSONArray
+
+class ListQuotesActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityListQuotesBinding
+
+    companion object {
+        private val TAG = ListQuotesActivity::class.java.simpleName
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityListQuotesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "List of Quotes"
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.listQuotes.layoutManager = layoutManager
+        val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
+        binding.listQuotes.addItemDecoration(itemDecoration)
+
+        getListQuotes()
+    }
+
+    private fun getListQuotes() {
+        binding.progressBar.visibility = View.VISIBLE
+        val client = AsyncHttpClient()
+        val url = "https://quote-api.dicoding.dev/list"
+        client.get(url, object : AsyncHttpResponseHandler() {
+
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?
+            ) {
+                binding.progressBar.visibility = View.INVISIBLE
+
+                val listQuotes = ArrayList<String>()
+
+                val result = responseBody?.let { String(it) }
+                result?.let { Log.d(TAG, it) }
+                try {
+                    val jsonArray = JSONArray(result)
+
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONObject(i)
+                        val quote = jsonObject.getString("en")
+                        val author = jsonObject.getString("author")
+                        listQuotes.add("\n$quote - $author\n")
+                    }
+
+                    val adapter = QuoteAdapter(listQuotes)
+                    binding.listQuotes.adapter = adapter
+                } catch (e: Exception) {
+                    Toast.makeText(this@ListQuotesActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                binding.progressBar.visibility = View.VISIBLE
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error?.message}"
+                }
+                Toast.makeText(this@ListQuotesActivity, errorMessage, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+}
